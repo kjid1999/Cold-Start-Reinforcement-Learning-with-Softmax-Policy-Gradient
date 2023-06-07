805118743fe4cdb4e650ca9101d0b5f3662f2f0f
kjid1999 <kjid1999@gmail.com>
2023-06-07 19:34:29 +0800

    稍微整理code 有問題的地方標了註解 很多地方加了self.上去

--- reward.py
+++ reward.py
@@ -15,29 +15,31 @@ def EOS(voc, t, target_sentence, end_token):
     return out
 
 class Updater:
-    def __init__(self, model, optimizer, R: function, p_drop=0.5, W=10000, J=1) -> None:
+    def __init__(self, model, optimizer, R: function, vocabulary, p_drop=0.5, W=10000, J=1) -> None:
         self.p_drop = p_drop
         self.W = W
         self.J = J
         self.optimizer = optimizer
         self.R = R
         self.model = model
+        self.voc = vocabulary
 
     def updata(self, x, y):
         L_BBSPG = 0
-        for j in range(1, J+1):
+        for j in range(1, self.J+1):
             z = []
             for t in range(1, self.T+1):
                 mu = torch.rand((1, ))
 
-                model_output = self.model()
-                if mu > p_drop:
+                model_output = self.model() # what should be input to model? x? 
+                if mu > self.p_drop:
                     # 參數傳入也許再改
-                    delta_r = W * (self.R(z, voc, t, y) - self.R(z, voc, t-1, y) + DUP(z, voc) + EOS(voc, t, y, eos))
+                    # eos 應該可以從voc得到 所以應該寫在EOS裡而不虛傳入
+                    delta_r = self.W * (self.R(z, self.voc, t, y) - self.R(z, self.voc, t-1, y) + DUP(z, self.voc) + EOS(self.voc, t, y, eos))
 
                     prob = softmax(torch.log(model_output) + delta_r)
                     zt_idx = Categorical(probs=prob).sample()
-                    z_t = voc[zt_idx]
+                    z_t = self.voc[zt_idx]
 
                     L_BBSPG = L_BBSPG.detach()
                     L_BBSPG -= torch.log(model_output[zt_idx])
@@ -45,33 +47,7 @@ class Updater:
                 
                 else:
                     prob = model_output
-                    z_t = voc[Categorical(probs=prob).sample()]
+                    z_t = self.voc[Categorical(probs=prob).sample()]
 
                 z.append(z_t)
         return
-
-p_drop=0.5
-W=10000
-J=1
-
-def f():
-    for j in range(1, J+1):
-        z = []
-        for t in range(1, T+1):
-            mu = torch.rand((1, ))
-
-            model_output = model()
-            if mu > p_drop:
-                delta_r = W * (R() - R() + DUP() + EOS())
-
-                prob = softmax(torch.log(model_output) + delta_r)
-                z_t = Categorical(probs=prob).sample()
-
-                L_BBSPG -= model_output
-                L_BBSPG.backward()
-            
-            else:
-                prob = model_output
-                z_t = Categorical(probs=prob).sample()
-
-            z.append(z_t)
