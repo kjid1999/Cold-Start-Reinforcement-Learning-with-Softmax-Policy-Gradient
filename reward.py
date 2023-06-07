61831e25502b9fcb0565ea63739f452435f590e6
kjid1999 <kjid1999@gmail.com>
2023-06-07 19:16:48 +0800

    DUP + EOS. Need to modify para pass

--- reward.py
+++ reward.py
@@ -2,8 +2,20 @@ import torch
 from torch.nn.functional import softmax
 from torch.distributions import Categorical
 
+def DUP(z, voc):
+    out = torch.zeros_like(voc)
+    out[voc == z[-1]] = -1
+    return out
+
+def EOS(voc, t, target_sentence, end_token):
+    out = torch.zeros_like(voc)
+    if t < len(target_sentence):
+        # voc == end_token 也許再改
+        out[voc == end_token] = -1
+    return out
+
 class Updater:
-    def __init__(self, model, optimizer, R, p_drop=0.5, W=10000, J=1) -> None:
+    def __init__(self, model, optimizer, R: function, p_drop=0.5, W=10000, J=1) -> None:
         self.p_drop = p_drop
         self.W = W
         self.J = J
@@ -11,9 +23,31 @@ class Updater:
         self.R = R
         self.model = model
 
-        
     def updata(self, x, y):
+        L_BBSPG = 0
+        for j in range(1, J+1):
+            z = []
+            for t in range(1, self.T+1):
+                mu = torch.rand((1, ))
 
+                model_output = self.model()
+                if mu > p_drop:
+                    # 參數傳入也許再改
+                    delta_r = W * (self.R(z, voc, t, y) - self.R(z, voc, t-1, y) + DUP(z, voc) + EOS(voc, t, y, eos))
+
+                    prob = softmax(torch.log(model_output) + delta_r)
+                    zt_idx = Categorical(probs=prob).sample()
+                    z_t = voc[zt_idx]
+
+                    L_BBSPG = L_BBSPG.detach()
+                    L_BBSPG -= torch.log(model_output[zt_idx])
+                    L_BBSPG.backward()
+                
+                else:
+                    prob = model_output
+                    z_t = voc[Categorical(probs=prob).sample()]
+
+                z.append(z_t)
         return
 
 p_drop=0.5
